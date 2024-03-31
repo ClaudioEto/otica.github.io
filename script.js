0ac137b1a61b87d0eba2f14b6d7c61a2b9b1e719
ClaudioEto <115515095+ClaudioEto@users.noreply.github.com>
2024-03-31 17:43:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function alert_success(){
+    Swal.fire({
+        title: "Obrigado!!",
+        text: "Sua mensagem/agendamento foi enviada! Retornaremos assim que possível",
+        icon: "success",
+        color: "#fff",
+        background: "#e26d5c",
+      });
+
+    
+}
